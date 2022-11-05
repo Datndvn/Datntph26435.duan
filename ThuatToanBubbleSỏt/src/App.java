a31beffa8ed8bada9670e77d18ff1177c17a0b7b
Your Name <you@example.com>
2022-11-05 21:43:07 +0700

    first commit

---
+++ ThuatToanBubbleSỏt/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] arrNumbers = {33,2,1,0};
+        //Xuất mảng chưa sort
+        for (int x: arrNumbers) {
+            System.out.print(x + " ");
+        }
+        System.out.println("");
+        //Thuật toán nổi bot
+        for (int i = 0; i < )
+    }
+}
